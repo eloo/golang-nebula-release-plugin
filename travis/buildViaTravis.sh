ba839be6e12a645fa8cbdfff46c5177a2c956650
Joseph Weigl <joseph.weigl@gmail.com>
2017-12-09 17:03:16 +0100

    test

--- travis/buildViaTravis.sh
+++ travis/buildViaTravis.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 # This script will build the project.
 
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo -e "Checkout branch [$TRAVIS_BRANCH]"
+  git checkout $TRAVIS_BRANCH
+fi
+
+
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
   ./gradlew goBuild
 elif [ "$TRAVIS_BRANCH" == "master" ] ; then
   echo -e 'Build Master Branch ['$TRAVIS_BRANCH']'
-  ./gradlew goBuild final sstatus ppush --info --stacktrace
+  ./gradlew goBuild final sstatus --info --stacktrace
 elif [ "$TRAVIS_BRANCH" == "develop" ] ; then
   echo -e 'Build Develop Branch ['$TRAVIS_BRANCH']'
   ./gradlew goBuild snapshot
